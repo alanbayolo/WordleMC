ee3b2788609e4d75ae23b826a90b7843b2182841
alanbayolo <60156381+alanbayolo@users.noreply.github.com>
2022-04-10 15:48:25 -0400

    write

--- src/main/java/com/teamcreeper/wordlemod/screen/WordleMenu.java
+++ src/main/java/com/teamcreeper/wordlemod/screen/WordleMenu.java
@@ -2,6 +2,7 @@ package com.teamcreeper.wordlemod.screen;
 
 import com.teamcreeper.wordlemod.block.ModBlocks;
 import com.teamcreeper.wordlemod.block.entity.WordleBlockEntity;
+import net.minecraft.core.BlockPos;
 import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.entity.player.Player;
@@ -12,49 +13,44 @@ import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraftforge.items.CapabilityItemHandler;
+import net.minecraftforge.items.ItemStackHandler;
 import net.minecraftforge.items.SlotItemHandler;
+import net.minecraftforge.items.ItemStackHandler;
+import net.minecraftforge.items.IItemHandler;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Supplier;
 
-public class WordleMenu extends AbstractContainerMenu {
+public class WordleMenu extends AbstractContainerMenu implements Supplier<Map<Integer, Slot>> {
+    public final static HashMap<String, Object> guistate = new HashMap<>();
     private final WordleBlockEntity blockEntity;
-    private final Level level;
+    public final Level world;
+    public final Player entity;
+    public int x,y,z;
+    private IItemHandler internal;
+    private final Map<Integer, Slot> customSlots = new HashMap<>();
+    private boolean bound = false;
 
     public WordleMenu(int windowId, Inventory inv, FriendlyByteBuf extraData) {
         this(windowId, inv, inv.player.level.getBlockEntity(extraData.readBlockPos()));
     }
 
-    public WordleMenu(int windowId, Inventory inv, BlockEntity entity) {
+    public WordleMenu(int windowId, Inventory inv, BlockEntity entity){
         super(ModMenuTypes.WORDLE_MENU.get(), windowId);
-        //checkContainerSize(inv, 4);
         blockEntity = ((WordleBlockEntity) entity);
-        this.level = inv.player.level;
-
-        addPlayerInventory(inv);
-        //addPlayerHotbar(inv);
-
-        /*this.blockEntity.getCapability(CapabilityItemHandler.ITEM_HANDLER_CAPABILITY).ifPresent(handler -> {
-            this.addSlot(new SlotItemHandler(handler, 1, 66, 16));
-            this.addSlot(new SlotItemHandler(handler, 2, 66, 50));
-        });*/
+        this.entity = inv.player;
+        this.world = inv.player.level;
+        this.internal = new ItemStackHandler(0);
+        BlockPos pos = null;
     }
 
     @Override
-    public boolean stillValid(Player pPlayer) {
-        return stillValid(ContainerLevelAccess.create(level, blockEntity.getBlockPos()),
-                pPlayer, ModBlocks.WORDLE_BLOCK.get());
+    public boolean stillValid(Player player){
+        return stillValid(ContainerLevelAccess.create(world, blockEntity.getBlockPos()),
+                player, ModBlocks.WORDLE_BLOCK.get());
     }
 
-    private void addPlayerInventory(Inventory playerInventory) {
-        for (int i = 0; i < 6; ++i) {
-            for (int l = 0; l < 5; ++l) {
-                this.addSlot(new Slot(playerInventory, l + i * 9 + 9, 42 + l * 19, 38 + i * 19));
-            }
-        }
+    public Map<Integer, Slot> get(){
+        return customSlots;
     }
-
-    /*private void addPlayerHotbar(Inventory playerInventory) {
-        for (int i = 0; i < 9; ++i) {
-            this.addSlot(new Slot(playerInventory, i, 8 + i * 18, 144));
-        }
-    }*/
-
 }
--- src/main/java/com/teamcreeper/wordlemod/screen/WordleScreen.java
+++ src/main/java/com/teamcreeper/wordlemod/screen/WordleScreen.java
@@ -8,15 +8,27 @@ import net.minecraft.client.renderer.GameRenderer;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.player.Inventory;
+import net.minecraftforge.client.event.InputEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+
+import java.awt.event.KeyEvent;
+
+import static java.awt.event.KeyEvent.VK_BACK_SPACE;
+import static java.awt.event.KeyEvent.VK_DELETE;
 
 public class WordleScreen extends AbstractContainerScreen<WordleMenu> {
-    private static final ResourceLocation TEXTURE =
-            new ResourceLocation(WordleMC.MOD_ID, "textures/gui/wordleboard.png");
+    private static final ResourceLocation TEXTURE = new ResourceLocation(WordleMC.MOD_ID, "textures/gui/wordleboard.png");
+    private static final ResourceLocation A = new ResourceLocation(WordleMC.MOD_ID, "textures/gui/a.png");
+    private static final ResourceLocation B = new ResourceLocation(WordleMC.MOD_ID, "textures/gui/b.png");
+    private static final ResourceLocation C = new ResourceLocation(WordleMC.MOD_ID, "textures/gui/c.png");
+    private static final ResourceLocation D = new ResourceLocation(WordleMC.MOD_ID, "textures/gui/d.png");
+    private static final ResourceLocation E = new ResourceLocation(WordleMC.MOD_ID, "textures/gui/e.png");
+    private static final ResourceLocation mid = new ResourceLocation(WordleMC.MOD_ID, "textures/gui/middle.png");
 
     public WordleScreen(WordleMenu pMenu, Inventory pPlayerInventory, Component pTitle) {
         super(pMenu, pPlayerInventory, pTitle);
     }
-
+    public static int a = 0;public static int b = 0;public static int c = 0;public static int d = 0;public static int e = 0;
     @Override
     protected void init() {
         super.init();
@@ -26,17 +38,74 @@ public class WordleScreen extends AbstractContainerScreen<WordleMenu> {
     protected void renderBg(PoseStack pPoseStack, float pPartialTicks, int pMouseX, int pMouseY) {
         RenderSystem.setShader(GameRenderer::getPositionTexShader);
         RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 1.0F);
-        RenderSystem.setShaderTexture(0, TEXTURE);
-        int x = (width - imageWidth) / 2;
-        int y = (height - imageHeight) / 2;
-
-        this.blit(pPoseStack, x, y, 0, 0, imageWidth, imageHeight);
     }
-
+    public static final String[] letters =  {String.valueOf(A), String.valueOf(B), String.valueOf(C), String.valueOf(D), String.valueOf(E)};
+    public static float typingPositions[][][];
     @Override
     public void render(PoseStack pPoseStack, int mouseX, int mouseY, float delta) {
         renderBackground(pPoseStack);
         super.render(pPoseStack, mouseX, mouseY, delta);
         renderTooltip(pPoseStack, mouseX, mouseY);
+        RenderSystem.setShaderTexture(0, TEXTURE);
+        int x = (width - imageWidth) / 2;
+        int y = (height - imageHeight) / 2;
+        this.blit(pPoseStack, x, y, 0, 0, imageWidth, imageHeight, imageWidth, imageHeight);
+
+        for (int i = 0; i < 6; ++i) {
+            for (int l = 0; l < 5; ++l) {
+                /*RenderSystem.setShaderTexture(0, mid);
+                this.blit(pPoseStack, this.leftPos + 32 + (l*23), this.topPos  + 32 + (i*22) ,0,0,20,20,20,20);*/
+
+                System.out.print("current pos X: " + this.leftPos + 32 + (l*23) + " current pos Y: " + this.topPos  + 32 + (i*22));
+                RenderSystem.setShaderTexture(0, ResourceLocation.tryParse(letters[l]));
+                this.blit(pPoseStack, this.leftPos + 32 + (l*23), this.topPos  + 32 + (i*22) ,0,0,20,20,20,20);
+
+
+            }
+        }
+
+        if(a != 0){
+            RenderSystem.setShaderTexture(0, mid);
+            this.blit(pPoseStack, this.leftPos + 32, this.topPos  + 32,0,0,20,20,20,20);
+            RenderSystem.setShaderTexture(0, D );
+            this.blit(pPoseStack, this.leftPos + 32, this.topPos + 32,0,0,20,20,20,20);
+        }
+        if(b != 0){
+            RenderSystem.setShaderTexture(0, mid);
+            this.blit(pPoseStack, this.leftPos + 55, this.topPos  + 32,0,0,20,20,20,20);
+            RenderSystem.setShaderTexture(0, D );
+            this.blit(pPoseStack, this.leftPos + 55, this.topPos  + 32,0,0,20,20,20,20);
+        }
+
+    }
+    public void TypingManager(){
+
+    }
+    public void typingRender(PoseStack pPoseStack,char typedChar){
+
+    }
+
+    //logic for adding the letters to an array every time a key is pressed 6x5
+    //after entering the array it generates an object of letter
+    //the object is then displayed
+    //function to remove the objects for the lastest letter in the array with the press of backspace
+    //function to execute the verification after pressing enter
+    //function for changing the background of the letters depending of their existance and correct positioning
+
+    @Override
+    public boolean charTyped(char typedChar, int keyCode){
+        //Pausing the game
+        if (typedChar == 'a' || typedChar == 'A'){
+            a = 1;
+        }
+        if (typedChar == 'b' || typedChar == 'B'){
+            a = 0;
+        }
+        if (typedChar == '/'){
+            a = 0;
+        }
+
+        super.charTyped(typedChar, keyCode);
+        return true;
     }
 }
